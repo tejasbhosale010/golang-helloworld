8c50d99418c93cafac8be128a33c97b16523c0a6
Tejas Bhosale <tejasbhosale010@gmail.com>
2024-07-05 21:13:24 +0530

    hello world code changed

--- main_test.go
+++ main_test.go
@@ -1,13 +1,36 @@
 package main
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
-func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+func TestHelloWorldHandler(t *testing.T) {
+	// Create a request to pass to the handler
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	// Create a ResponseRecorder to record the response
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(HelloWorldHandler)
+
+	// Serve the HTTP request to the ResponseRecorder
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body
+	expected := "Hello, World!"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
 	}
 }
 
